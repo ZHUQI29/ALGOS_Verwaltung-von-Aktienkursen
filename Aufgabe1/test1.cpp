4d250c73bfa717b965525e6c08318cf8200a8b53
ZHUQI29 <if21b206@technikum-wien.at>
2022-03-04 21:23:21 +0100

    try

--- Aufgabe1/test1.cpp
+++ Aufgabe1/test1.cpp
@@ -6,19 +6,22 @@
 
 using namespace std;
 
-
-int main()
-{
+void search(){
 	// 读文件 Datei lesen
 	std::cout << "Bitte Wertpapiere Abk eingeben" << '\n';
 	string str="/Users/zhuqi/Desktop/ALGOS/Aufgabe1/";//Dateipfad
 	string c;//Wertpapiere Abk
 	string csv=".csv";
- 	std::cin >> c;
- 	str+=c;
- 	str+=csv;
+	std::cin >> c;
+	str+=c;
+	str+=csv;
 	std::cout << str<<endl;
- 	ifstream inFile(str, ios::in);
+
+	ifstream inFile(str, ios::in);
+	if( inFile.fail( ) ) {
+            cerr << "Error: Datei nicht gefunden oder kann nicht geöffnet werden" << str <<endl<< endl;
+            search();  // Error;
+        }
 	// ifstream inFile("/Users/zhuqi/Desktop/ALGOS/Aufgabe1/MSFT.csv", ios::in);
 	string lineStr;
 	vector<vector<string> > strArray;
@@ -35,6 +38,30 @@ int main()
 			lineArray.push_back(str);
 		strArray.push_back(lineArray);
 	}
+}
+char frage(){
+	char a;
+	std::cout << "1. ADD 2. DEL 3. IMPORT 4. SEARCH 5. PLOT 6. SAVE 7. LOAD 8. QUIT" << '\n';
+	std::cin >>a;
+	return a;
+}
+
+int main()
+{
+	int i=1;
+	while(i){
+		switch (frage()) {
+			case '1':std::cerr << "noch nicht schreiben" << '\n';break;
+			case '2':std::cerr << "noch nicht schreiben" << '\n';break;
+			case '3':std::cerr << "noch nicht schreiben" << '\n';break;
+			case '4':search();break;
+			case '5':std::cerr << "noch nicht schreiben" << '\n';break;
+			case '6':std::cerr << "noch nicht schreiben" << '\n';break;
+			case '7':std::cerr << "noch nicht schreiben" << '\n';break;
+			case '8':i=0;break;
+		}
+	}
+
 
 	getchar();
 	return 0;
