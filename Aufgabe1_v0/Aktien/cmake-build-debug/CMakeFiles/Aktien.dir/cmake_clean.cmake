ad9ac4a9209d0f11ff52a07e0071b1bd2e986346
ZHUQI29 <if21b206@technikum-wien.at>
2022-03-15 14:07:06 +0100

    Namen der Aktie und WKN

---
+++ Aufgabe1_v0/Aktien/cmake-build-debug/CMakeFiles/Aktien.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "Aktien"
+  "Aktien.pdb"
+  "CMakeFiles/Aktien.dir/HashTable.cpp.o"
+  "CMakeFiles/Aktien.dir/HashTable.cpp.o.d"
+  "CMakeFiles/Aktien.dir/Holder.cpp.o"
+  "CMakeFiles/Aktien.dir/Holder.cpp.o.d"
+  "CMakeFiles/Aktien.dir/Stock.cpp.o"
+  "CMakeFiles/Aktien.dir/Stock.cpp.o.d"
+  "CMakeFiles/Aktien.dir/StockEntry.cpp.o"
+  "CMakeFiles/Aktien.dir/StockEntry.cpp.o.d"
+  "CMakeFiles/Aktien.dir/main.cpp.o"
+  "CMakeFiles/Aktien.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aktien.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Aufgabe1_v0/Aktien/cmake-build-debug/CMakeFiles/Aktien.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Aktien.
+# This may be replaced when dependencies are built.
